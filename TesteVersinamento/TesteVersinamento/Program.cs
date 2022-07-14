fe8589e8092cd19a0d43a2b3386ee228ee5a4428
Marcos Santos <marcos.sbnf@gmail.com>
2022-07-13 22:07:20 -0300

    instanciado classe class1

--- TesteVersinamento/TesteVersinamento/Program.cs
+++ TesteVersinamento/TesteVersinamento/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Busisness;
+
 Console.WriteLine("Hello, World!");
+
+var escrever = new Class1("o texto de parametro");
+
+Console.WriteLine(value: escrever.RetornaUmTexto);
+Console.ReadKey();
